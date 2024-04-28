d9fddfd66947a38f3255a046d80d2a9d6a0c2a8c
pedro <151687035+pedrokunrath@users.noreply.github.com>
2024-04-28 02:02:29 -0300

    first commit

---
+++ redacao.py
@@ -0,0 +1,77 @@
+import pyautogui
+import time
+
+
+# Aguarda 5 segundos antes de começar
+time.sleep(5)
+
+
+
+# abrir o navegador (chrome)
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+time.sleep(2)
+pyautogui.click(x=622, y=441)
+time.sleep(2)
+pyautogui.click(x=93, y=561)
+time.sleep(1)
+
+#paragrafo1
+pyautogui.typewrite("")
+
+
+pyautogui.press("enter")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+
+#paragrafo2
+pyautogui.typewrite("")
+
+pyautogui.press("enter")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+#paragrafo3
+pyautogui.typewrite("")
+
+pyautogui.press("enter")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+
+#paragrafo4
+pyautogui.typewrite("")
+
+pyautogui.press("enter")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+
+#paragrafo5
+pyautogui.typewrite("")
+
+
+pyautogui.press("enter")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+pyautogui.press("space")
+
+#paragrafo6
+pyautogui.typewrite("")
